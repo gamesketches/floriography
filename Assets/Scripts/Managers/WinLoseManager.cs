2fd8d73e33d1d2dbf2dbc102d5a4b5fe378acf1c
tsarachan <test@notanemailaddress.com>
2016-06-21 18:48:52 -0400

    Player prefab, victory condition
    Created prefab for player, with model, and scripts to allow the player
    to win the game.

---
+++ Assets/Scripts/Managers/WinLoseManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinLoseManager : MonoBehaviour {
+
+	public void PlayerWins()
+	{
+		Debug.Log("You win! Happiness and joy are yours.");
+	}
+
+	public void PlayerLoses()
+	{
+		Debug.Log("You lose! The hopelessness of your love is spirit-crushing.");
+	}
+}
---
+++ Assets/Scripts/Player/WinTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinTest : MonoBehaviour {
+
+	WinLoseManager winLoseManager;
+
+	public void InitializeVariables()
+	{
+		winLoseManager = transform.root.Find("Scene manager").GetComponent<WinLoseManager>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag.Contains("Girl")) { winLoseManager.PlayerWins(); }
+	}
+}
